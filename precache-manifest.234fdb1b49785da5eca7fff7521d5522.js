ac2dcb2f85f41e25fb3649964f29d6c329d90a3d
Andres Del Rio <addelrio11@gmail.com>
2021-09-25 17:29:20 -0400

    Updates

---
+++ precache-manifest.234fdb1b49785da5eca7fff7521d5522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24525c2a7deb363e17fca9fec1556fa8",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "f6914ae64ec8b0fa942b",
+    "url": "/twitter-clone/static/css/main.a183041b.chunk.css"
+  },
+  {
+    "revision": "b658db56de51092bc5bf",
+    "url": "/twitter-clone/static/js/2.c185b38f.chunk.js"
+  },
+  {
+    "revision": "5646fed769b63e8695677f17dadd06b5",
+    "url": "/twitter-clone/static/js/2.c185b38f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6914ae64ec8b0fa942b",
+    "url": "/twitter-clone/static/js/main.d124e898.chunk.js"
+  },
+  {
+    "revision": "4ea1c8ca6c4e7890228d",
+    "url": "/twitter-clone/static/js/runtime-main.a74d6e56.js"
+  }
+]);
